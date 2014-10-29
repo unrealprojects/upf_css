9784b0491cf6b64ea6a50665b86a11dc75e2deda
a <a>
2014-10-29 16:41:32 +0200

    Mayhem

--- Template.php
+++ Template.php
@@ -19,14 +19,78 @@
 
     </head>
     <body>
-        <section class="Node">
-            <div class="Input-Select">
-                <input type="text"/>
-                <ul class="Input-Select-Content">
-                    <li><a href="#">asd</a></li>
-                </ul>
-            </div>
-        </section>
+    <div class="Page-Wrapper">
+    <nav class="Page-Header">
+        <script>
+            $(document).ready(function(){
+                $('Navicon-Toggle').click(function(){
+                    alert('a');
+                });
+            });
+
+        </script>
+        <div class="Navicon-Toggle Spinning White Visible-Mobile Hidden-XS">
+            <span class="Icon"></span>
+        </div>
+        <ul class="Page-Navigation Visible-XS">
+            <li class="Page-Navigation-Item"><a href="#">About</a></li>
+            <li class="Page-Navigation-Item"><a href="#">Grid</a></li>
+            <li class="Page-Navigation-Item"><a href="#">Forms</a></li>
+            <li class="Page-Navigation-Item"><a href="#">Typography</a></li>
+            <li class="Page-Navigation-Item"><a href="#">Test Room</a></li>
+        </ul>
+    </nav>
+
+        <aside>
+
+        </aside>
+        <main>
+            <section class="Node">
+                <h3 class="Underlined">Framework Variables</h3>
+                <p>
+                    <code class="Inline">$Core-Grid-Enable</code> Toggles <strong>Grid Generator</strong>
+                </p>
+                <p>
+                    <code class="Inline">$Core-Reset-Enable</code> Toggles global reset
+                </p>
+            </section>
+            <section class="Node">
+                <h3 class="Underlined">Testing page</h3>
+                <p class="Leading">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae quae velit fugit tempora doloremque veritatis possimus magnam error.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae quae velit fugit tempora doloremque veritatis possimus magnam error, ab ratione, quia minus vero nesciunt deleniti, dolore sunt. Iure, quo, animi.
+                </p>
+                <p class="Secondary">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae quae velit fugit tempora doloremque veritatis possimus magnam error, ab ratione, quia minus vero nesciunt deleniti, dolore sunt. Iure, quo, animi.
+                </p>
+            </section>
+            <section class="Node">
+                <form>
+                    <h4 class="Underlined">Form testing</h4>
+                    <div class="Control-Group">
+                        <label class="Node-XS-1-4" for="demo-1-1">Consectetur adipisicing</label>
+                        <input class="Node-XS-3-4" id="demo-1-1" type="text"/>
+                    </div>
+                    <div class="Control-Group">
+                        <label class="Node-XS-1-4" for="demo-1-2">Molestiae quae</label>
+                        <input class="Node-XS-3-4" id="demo-1-2" type="text"/>
+                    </div>
+                    <div class="Control-Group">
+                        <button class="Button">I'm a button</button>
+                    </div>
+                </form>
+            </section>
+        </main>
+        <hr class="Page-Wrapper-Stop"/>
+    </div>
+    <footer class="Page-Footer" role="contentinfo">
+        &copy; 2014 Unreal Projects Framework
+    </footer>
+
+
+        <!--
         <section class="Grid Split Multi-Row Test">
             <h1>Grid Split Multi-Row</h1>
             <div class="Node-XS-3">Placeholder Images for every case.</div>
@@ -88,6 +152,5 @@
             <div class="Node-3-7">Placeholder Images for every case.</div>
             <div class="Node-2-7">Placeholder Images for every case.</div>
         </section>
-
-
+        -->
 </html>
