7238094a68b493c959b67e81f7af3aa652100aeb
Олег <office@unrealprojects.com>
2014-10-29 17:40:55 +0200

    1

---
+++ pages/grid.php
@@ -0,0 +1,37 @@
+<section class="Node">
+    <h3 class="Underlined">Framework Variables</h3>
+    <p>
+        <code class="Inline">$Core-Grid-Enable</code> Toggles <strong>Grid Generator</strong>
+    </p>
+    <p>
+        <code class="Inline">$Core-Reset-Enable</code> Toggles global reset
+    </p>
+</section>
+<section class="Node">
+    <h3 class="Underlined">Testing page</h3>
+    <p class="Leading">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae quae velit fugit tempora doloremque veritatis possimus magnam error.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae quae velit fugit tempora doloremque veritatis possimus magnam error, ab ratione, quia minus vero nesciunt deleniti, dolore sunt. Iure, quo, animi.
+    </p>
+    <p class="Secondary">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae quae velit fugit tempora doloremque veritatis possimus magnam error, ab ratione, quia minus vero nesciunt deleniti, dolore sunt. Iure, quo, animi.
+    </p>
+</section>
+<section class="Node">
+    <form>
+        <h4 class="Underlined">Form testing</h4>
+        <div class="Control-Group">
+            <label class="Node-XS-1-4" for="demo-1-1">Consectetur adipisicing</label>
+            <input class="Node-XS-3-4" id="demo-1-1" type="text"/>
+        </div>
+        <div class="Control-Group">
+            <label class="Node-XS-1-4" for="demo-1-2">Molestiae quae</label>
+            <input class="Node-XS-3-4" id="demo-1-2" type="text"/>
+        </div>
+        <div class="Control-Group">
+            <button class="Button">I'm a button</button>
+        </div>
+    </form>
+</section>
