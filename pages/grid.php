d4c1d5e8fd7277f6e14334b5e90da698b81d9cac
a <a>
2014-10-29 23:05:48 +0200

    Mayhem

---
+++ pages/grid.php
@@ -0,0 +1,189 @@
+<section class="Node-Out">
+    <div class="Node-Ghost">
+        <h3 class="Underlined">Fraction</h3>
+<pre><code>&lt;div class="Grid Demo"&gt;
+    &lt;div class="Col-1-2"&gt;1/2&lt;/div&gt;
+    &lt;div class="Col-1-2"&gt;1/2&lt;/div&gt;
+&lt;/div&gt;</code></pre>
+        <div class="Grid Demo">
+            <div class="Col-1-2">1/2</div>
+            <div class="Col-1-2">1/2</div>
+        </div>
+        <div class="Grid Demo">
+            <div class="Col-1-3">1/3</div>
+            <div class="Col-1-3">1/3</div>
+            <div class="Col-1-3">1/3</div>
+        </div>
+        <div class="Grid Demo">
+            <div class="Col-1-4">1/4</div>
+            <div class="Col-1-4">1/4</div>
+            <div class="Col-1-4">1/4</div>
+            <div class="Col-1-4">1/4</div>
+        </div>
+        <div class="Grid Demo">
+            <div class="Col-1-5">1/5</div>
+            <div class="Col-1-5">1/5</div>
+            <div class="Col-1-5">1/5</div>
+            <div class="Col-1-5">1/5</div>
+            <div class="Col-1-5">1/5</div>
+        </div>
+        <div class="Grid Demo">
+            <div class="Col-1-6">1/6</div>
+            <div class="Col-1-6">1/6</div>
+            <div class="Col-1-6">1/6</div>
+            <div class="Col-1-6">1/6</div>
+            <div class="Col-1-6">1/6</div>
+            <div class="Col-1-6">1/6</div>
+        </div>
+        <div class="Grid Demo">
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+        </div>
+        <div class="Grid Demo">
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+        </div>
+
+
+        <h3 class="Underlined">Split Fraction</h3>
+        <div class="Grid Split Demo">
+            <div class="Col-1-2">1/2</div>
+            <div class="Col-1-2">1/2</div>
+        </div>
+        <div class="Grid Split Demo">
+            <div class="Col-1-3">1/3</div>
+            <div class="Col-1-3">1/3</div>
+            <div class="Col-1-3">1/3</div>
+        </div>
+        <div class="Grid Split Demo">
+            <div class="Col-1-4">1/4</div>
+            <div class="Col-1-4">1/4</div>
+            <div class="Col-1-4">1/4</div>
+            <div class="Col-1-4">1/4</div>
+        </div>
+        <div class="Grid Split Demo">
+            <div class="Col-1-5">1/5</div>
+            <div class="Col-1-5">1/5</div>
+            <div class="Col-1-5">1/5</div>
+            <div class="Col-1-5">1/5</div>
+            <div class="Col-1-5">1/5</div>
+        </div>
+        <div class="Grid Split Demo">
+            <div class="Col-1-6">1/6</div>
+            <div class="Col-1-6">1/6</div>
+            <div class="Col-1-6">1/6</div>
+            <div class="Col-1-6">1/6</div>
+            <div class="Col-1-6">1/6</div>
+            <div class="Col-1-6">1/6</div>
+        </div>
+        <div class="Grid Split Demo">
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+            <div class="Col-1-7">1/7</div>
+        </div>
+        <div class="Grid Split Demo">
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+            <div class="Col-1-8">1/8</div>
+        </div>
+
+        <h3 class="Underlined">Multicolumn</h3>
+        <div class="Grid Demo">
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+        </div>
+        <div class="Grid Demo">
+            <div class="Col-2">2</div>
+            <div class="Col-2">2</div>
+            <div class="Col-2">2</div>
+            <div class="Col-2">2</div>
+            <div class="Col-2">2</div>
+            <div class="Col-2">2</div>
+        </div>
+        <div class="Grid Demo">
+            <div class="Col-3">3</div>
+            <div class="Col-3">3</div>
+            <div class="Col-3">3</div>
+            <div class="Col-3">3</div>
+        </div>
+        <div class="Grid Demo">
+            <div class="Col-4">4</div>
+            <div class="Col-4">4</div>
+            <div class="Col-4">4</div>
+        </div>
+        <div class="Grid Demo">
+            <div class="Col-6">6</div>
+            <div class="Col-6">6</div>
+        </div>       
+        
+        
+        <h3 class="Underlined">Split Multicolumn</h3>
+        <div class="Grid Split Demo">
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+            <div class="Col-1">1</div>
+        </div>
+        <div class="Grid Split Demo">
+            <div class="Col-2">2</div>
+            <div class="Col-2">2</div>
+            <div class="Col-2">2</div>
+            <div class="Col-2">2</div>
+            <div class="Col-2">2</div>
+            <div class="Col-2">2</div>
+        </div>
+        <div class="Grid Split Demo">
+            <div class="Col-3">3</div>
+            <div class="Col-3">3</div>
+            <div class="Col-3">3</div>
+            <div class="Col-3">3</div>
+        </div>
+        <div class="Grid Split Demo">
+            <div class="Col-4">4</div>
+            <div class="Col-4">4</div>
+            <div class="Col-4">4</div>
+        </div>
+        <div class="Grid Split Demo">
+            <div class="Col-6">6</div>
+            <div class="Col-6">6</div>
+        </div>
+    </div>
+</section>
