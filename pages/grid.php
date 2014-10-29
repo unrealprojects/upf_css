196e002203dd7ecd287fb3f7ad218be81c16f175
Олег <office@unrealprojects.com>
2014-10-29 23:52:58 +0200

    1

--- pages/grid.php
+++ pages/grid.php
@@ -1,10 +1,12 @@
 <section class="Node-Out">
     <div class="Node-Ghost">
         <h3 class="Underlined">Fraction</h3>
-<pre><code>&lt;div class="Grid Demo"&gt;
+<pre><code>
+&lt;div class="Grid Demo"&gt;
     &lt;div class="Col-1-2"&gt;1/2&lt;/div&gt;
     &lt;div class="Col-1-2"&gt;1/2&lt;/div&gt;
-&lt;/div&gt;</code></pre>
+&lt;/div&gt;
+</code></pre>
         <div class="Grid Demo">
             <div class="Col-1-2">1/2</div>
             <div class="Col-1-2">1/2</div>
@@ -185,43 +187,46 @@
             <div class="Col-6">6</div>
             <div class="Col-6">6</div>
         </div>
+
+
+        <h3 class="Underlined">Vertical Column</h3>
+        <div class="Grid Vertical Multi-Row Demo">
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/200/abstract/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/300/animals/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/400/sports/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/500/nature/" alt="Image"/></div>
+
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/800/abstract/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/500/animals/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/400/sports/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/200/nature/" alt="Image"/></div>
+
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/300/abstract/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/400/animals/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/500/sports/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/600/nature/" alt="Image"/></div>
+        </div>
+
+        <h3 class="Underlined">Split Vertical Column</h3>
+        <div class="Grid Vertical Multi-Row Split Demo">
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/200/abstract/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/300/animals/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/400/sports/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/500/nature/" alt="Image"/></div>
+
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/800/abstract/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/500/animals/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/400/sports/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/200/nature/" alt="Image"/></div>
+
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/300/abstract/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/400/animals/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/500/sports/" alt="Image"/></div>
+            <div class="Col-3"><img src="http://lorempixel.com/g/600/600/nature/" alt="Image"/></div>
+        </div>
+
     </div>
 </section>
 
-<section class="Node">
-    <article class="Grid Vertical Multi-Row Split">
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/200/abstract/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/300/animals/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/400/sports/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/500/nature/" alt="Image"/></div>
 
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/800/abstract/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/500/animals/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/400/sports/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/200/nature/" alt="Image"/></div>
 
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/300/abstract/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/400/animals/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/500/sports/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/600/nature/" alt="Image"/></div>
-    </article>
-</section>
-
-<section class="Node">
-    <article class="Grid Vertical Multi-Row">
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/200/abstract/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/300/animals/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/400/sports/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/500/nature/" alt="Image"/></div>
-
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/800/abstract/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/500/animals/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/400/sports/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/200/nature/" alt="Image"/></div>
-
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/300/abstract/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/400/animals/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/500/sports/" alt="Image"/></div>
-        <div class="Col-3"><img src="http://lorempixel.com/g/600/600/nature/" alt="Image"/></div>
-    </article>
-</section>
